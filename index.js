153e71def0e1c20d48dba01f542a7420363d0a8f
Taro Seo <mail2017@seotaro.com>
2021-02-10 11:56:12 +0900

    refactoring

--- index.js
+++ index.js
@@ -28,29 +28,16 @@ app.post('/', upload.single('thumb'), asyncRoute(async (req, res, next) => {
 
   // レートが変更したらtwitter, slackに投稿する。
   if (payload.event == 'media.rate') {
-    let buffer;
+    let thumbneil;
     if (req.file && req.file.buffer) {
-      buffer = req.file.buffer;
+      thumbneil = req.file.buffer;
 
     } else if (payload.thumb) {
-      buffer = await request.get({
+      thumbneil = await request.get({
         uri: payload.thumb,
         encoding: null
       });
     }
-
-    if (buffer) {
-      // image = await sharp(buffer)
-      //   .resize({
-      //     height: 75,
-      //     width: 75,
-      //     fit: 'contain',
-      //     background: 'white'
-      //   })
-      //   .toBuffer();
-    }
-
-    console.log('media.rate', payload.Metadata.originalTitle, payload.Metadata.grandparentTitle, payload.Metadata.parentTitle, payload.Metadata.title, payload.rating);
 
     const stars = ("★".repeat(Number(payload.rating) / 2) + "☆☆☆☆☆").substr(0, 5)  // payload.rating は10段階。★は5段階で表現する。
     const message = "#NowRating " + stars + "\n" +
@@ -58,7 +45,7 @@ app.post('/', upload.single('thumb'), asyncRoute(async (req, res, next) => {
 
     // post to twitter
     (async () => {
-      client.post('media/upload', { media: buffer }, function (error, media, response) {
+      client.post('media/upload', { media: thumbneil }, function (error, media, response) {
         if (!error) {
           console.log(media);
           const status = {
